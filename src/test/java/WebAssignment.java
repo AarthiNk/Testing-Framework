03eedd4b2e7e380d07a58be175d6172d66da141e
AarthiNk <105538789+AarthiNk@users.noreply.github.com>
2022-05-17 23:46:23 +0530

    Update WebAssignment.java
    contants & utilities added

--- src/test/java/WebAssignment.java
+++ src/test/java/WebAssignment.java
@@ -1,3 +1,5 @@
+import constants.UrlConstants;
+import constants.WebElementsConstants;
 import io.qameta.allure.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,7 +12,12 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
+import static utils.DriverUtils.*;
 
+/**
+ * For web automation
+ * @author Aarthi
+ */
 @Epic("Web Automation")
 @Feature("Jquery suite testings")
 public class WebAssignment {
@@ -19,17 +26,22 @@ public class WebAssignment {
 
         WebElement controlGroupElement;
 
-        @BeforeTest
+    /**
+     * To setup the chrome driver and open the site in a browser
+     * @author Aarthi
+     * @throws InterruptedException
+     */
+    @BeforeTest
         public void setup() throws InterruptedException {
             synchronized (driver) {
-                driver.get("https://jqueryui.com/");
+                driver.get(UrlConstants.WEB_AUTOMATION_URL);
                 driver.wait(100);
-                driver.findElements(new By.ByClassName("widget")).forEach(webElement -> {
-                    boolean isInteractionElement = webElement.findElement(new By.ByClassName("widget-title")).getText().equalsIgnoreCase("Interactions");
+                findElementsByClass(driver,WebElementsConstants.WIDGET).forEach(webElement -> {
+                    boolean isInteractionElement = findElementByClass(webElement,WebElementsConstants.WIDGET_TITLE).getText().equalsIgnoreCase("Interactions");
                     if(isInteractionElement){
                          interactionElement = webElement;
                     }
-                    boolean isControlElement = webElement.findElement(new By.ByClassName("widget-title")).getText().equalsIgnoreCase("Widgets");
+                    boolean isControlElement = findElementByClass(webElement,WebElementsConstants.WIDGET_TITLE).getText().equalsIgnoreCase("Widgets");
                     if(isControlElement){
                          controlGroupElement = webElement;
                     }
@@ -37,26 +49,35 @@ public class WebAssignment {
             }
         }
 
-        @AfterTest
+    /**
+     * To Quit the browser
+     * @throws InterruptedException
+     */
+    @AfterTest
         public void setDown() throws InterruptedException {
             synchronized (driver) {
                 driver.quit();
             }
         }
 
-        @Test (priority = 0, description="Invalid Login Scenario with wrong username and password.")
+    /**
+     * To open the droppable and drag and drop
+     * @author Aarthi
+     * @throws InterruptedException
+     */
+    @Test (priority = 0, description="sample description for Report")
         @Severity(SeverityLevel.CRITICAL)
         @Step("Go to droppable and drag & drop")
         public void testCaseDragAndDrop() throws InterruptedException {
             synchronized (driver)
             {
-            interactionElement.findElement(new By.ByTagName("ul")).findElements(new By.ByTagName("li")).get(1).click();
+            getElementByTagName(interactionElement,  WebElementsConstants.UL).findElements(new By.ByTagName( WebElementsConstants.LI)).get(1).click();
             driver.wait(1000);
             driver.switchTo().frame(0);
             //WebElement on which drag and drop operation needs to be performed
-            WebElement fromElement = driver.findElement(new By.ById("draggable"));
+            WebElement fromElement = findElementByID(driver,WebElementsConstants.DRAGGABLE);
             //WebElement to which the above object is dropped
-            WebElement toElement = driver.findElement(new By.ById("droppable"));
+            WebElement toElement = findElementByID(driver,WebElementsConstants.DROPPABLE);
             //Creating object of Actions class to build composite actions
             Actions builder = new Actions(driver);
             //Building a drag and drop action
@@ -65,22 +86,25 @@ public class WebAssignment {
                     .release(toElement)
                     .build().perform();
 
-            String verifyText = driver.findElement(new By.ById("droppable")).findElement(new By.ByTagName("P")).getText();
+            String verifyText = findElementByID(driver,WebElementsConstants.DROPPABLE).findElement(new By.ByTagName("P")).getText();
             Assert.assertEquals(verifyText,"Dropped!");
             validator(driver, "Droppable | jQuery UI");
             }
         }
 
 
-
+    /**
+     * To open selectable and selct item 1 3 & 7
+     * @throws InterruptedException
+     */
     @Test
     public void testCaseSelectItems() throws InterruptedException {
         synchronized (driver)
         {
-            interactionElement.findElement(new By.ByTagName("ul")).findElements(new By.ByTagName("li")).get(3).click();
+            interactionElement.findElement(new By.ByTagName( WebElementsConstants.UL)).findElements(new By.ByTagName( WebElementsConstants.LI)).get(3).click();
             driver.wait(1000);
             driver.switchTo().frame(0);
-            List<WebElement> items = driver.findElement(new By.ById("selectable")).findElements(new By.ByTagName("li"));
+            List<WebElement> items = findElementByID(driver,WebElementsConstants.SELECTABLE).findElements(new By.ByTagName( WebElementsConstants.LI));
 
             items.forEach((webElement) -> {
                 if(webElement.getText().equalsIgnoreCase("Item 1")|| webElement.getText().equalsIgnoreCase("Item 3") || webElement.getText().equalsIgnoreCase("Item 7")){
@@ -90,7 +114,7 @@ public class WebAssignment {
                 }
             });
             driver.wait(1000);
-            String firstItem = driver.findElements(new By.ByClassName("ui-selected")).get(0).getText();
+            String firstItem = findElementsByClass(driver,"ui-selected").get(0).getText();
             Assert.assertEquals(firstItem, "Item 1");
             validator(driver, "Selectable | jQuery UI");
         }
@@ -101,11 +125,11 @@ public class WebAssignment {
     public void testCaseControlGroup() throws InterruptedException {
         synchronized (driver)
         {
-            controlGroupElement.findElement(new By.ByTagName("ul")).findElements(new By.ByTagName("li")).get(4).click();
+            getElementByTagName(controlGroupElement,  WebElementsConstants.UL).findElements(new By.ByTagName( WebElementsConstants.LI)).get(4).click();
             driver.wait(1000);
             driver.switchTo().frame(0);
 
-            WebElement selectElement = driver.findElement(new By.ById("car-type"));
+            WebElement selectElement = findElementByID(driver,"car-type");
             ((JavascriptExecutor) driver).executeScript("document.getElementById('car-type').style.display='block';");
             selectElement.click();
             Select s = new Select(selectElement);
@@ -119,8 +143,14 @@ public class WebAssignment {
         }
     }
 
+
+    /**
+     * To iterate web element
+     * @param className
+     * @param values
+     */
     private void selectValues(String className, String values) {
-        List<WebElement> verticalItems = driver.findElement(new By.ByClassName(className)).findElements(new By.ByXPath("label"));
+        List<WebElement> verticalItems = findElementByClass(driver,className).findElements(new By.ByXPath("label"));
 
         verticalItems.forEach((webElement) -> {
             if(webElement.getText().equalsIgnoreCase(values) || webElement.getText().equalsIgnoreCase("Insurance")){
@@ -129,6 +159,12 @@ public class WebAssignment {
         });
     }
 
+    /**
+     * To validate the output
+     * @param driver
+     * @param title
+     * @throws InterruptedException
+     */
     private void validator(WebDriver driver, String title) throws InterruptedException {
         Assert.assertEquals(driver.getTitle(), title);
         driver.navigate().back();
